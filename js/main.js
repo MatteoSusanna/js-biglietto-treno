67438ae7bab6048d61d60c4197c1f17546d1cc46
Matteo Susanna <matteo2784@gmail.com>
2022-07-04 15:35:44 +0200

    correzione

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ const tot = numeroKm * 0.21;
 
 
 if (eta < 18){
-    alert ('il prezzo sarà di ' + (tot.toFixed(2)) - (numeroKm * 20 / 100) + ' €');
+    alert ('il prezzo sarà di ' + (tot) - (numeroKm * 20 / 100) + ' €');
 
 } else if (eta > 65) {
-    alert ('il prezzo sarà di ' + (tot.toFixed(2)) - (numeroKm * 40 / 100) + ' €');
+    alert ('il prezzo sarà di ' + (tot) - (numeroKm * 40 / 100) + ' €');
 
 } else{
-    alert ('il prezzo sarà di ' + (tot.toFixed(2)) + ' €');
+    alert ('il prezzo sarà di ' + (tot) + ' €');
 }
