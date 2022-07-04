19092a81409c280e2410a6cbc9039448b899e52f
Matteo Susanna <matteo2784@gmail.com>
2022-07-04 16:20:16 +0200

    Prova

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ const eta = parseInt(prompt('Inserisci la tua età'));
 
 const tot = numeroKm * 0.21;
 
-let totminori = parseInt();
-
 
 
 if (eta < 18){
-    alert((totminori.toFixed(2)) = 'il prezzo sarà di ' + (tot - (tot * 20 / 100)) + ' € (sconto 20%)');
+    alert ('il prezzo sarà di ' + (tot - (tot * 20 / 100)) + ' € (sconto 20%)');
 
 } else if (eta > 65) {
     alert ('il prezzo sarà di ' + (tot - (tot * 40 / 100)) + ' € (sconto 40%)');
