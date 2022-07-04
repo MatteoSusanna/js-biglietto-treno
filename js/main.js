ca8bfa0796a6bb67d4b7499815f4fcd10e6e4c1c
Matteo Susanna <matteo2784@gmail.com>
2022-07-04 15:06:44 +0200

    due decimali

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ const eta = parseInt(prompt('Inserisci la tua età'));
 const tot = numeroKm * 0.21;
 
 
+
 if (eta < 18){
-    alert ('il prezzo sarà di ' + (tot) - (numeroKm * 20 / 100) + ' €');
+    alert ('il prezzo sarà di ' + (tot.toFixed(2)) - (numeroKm * 20 / 100) + ' €');
 
 } else if (eta > 65) {
-    alert ('il prezzo sarà di ' + (tot) - (numeroKm * 40 / 100) + ' €');
+    alert ('il prezzo sarà di ' + (tot.toFixed(2)) - (numeroKm * 40 / 100) + ' €');
 
 } else{
-    alert ('il prezzo sarà di ' + (tot) + ' €');
+    alert ('il prezzo sarà di ' + (tot.toFixed(2)) + ' €');
 }
